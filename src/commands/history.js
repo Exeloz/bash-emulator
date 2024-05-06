8e4fcd95659020672b560cff63ffc4ea04247edd
exeloz <christophercoulombe@gmail.com>
2024-05-05 20:53:50 -0400

    Adding parameter to history

--- src/commands/history.js
+++ src/commands/history.js
@@ -1,14 +1,21 @@
-var lineNumber = require('../utils/lineNumber')
+var addLineNumbers = require('../utils/lineNumber');
 
-// default width for number column
-var numColumnWidth = 5
+// Default width for number column
+var numColumnWidth = 5;
 
-function history (env) {
-  env.system.getHistory().then(function (history) {
-    env.output(lineNumber.addLineNumbers(numColumnWidth, history).join('\n'))
-    env.exit()
-  })
+function history(env, args) {
+  var splicedHistoryLength = args.length === 1 ? null : args[1];
+
+  env.system.getHistory().then(function(history) {
+    var splicedHistory = history.filter(function(element, index) {
+      if (!splicedHistoryLength || index >= history.length - splicedHistoryLength) {
+        return true;
+      }
+      return false;
+    });
+    env.output(addLineNumbers(numColumnWidth, splicedHistory).join('\n'));
+    env.exit();
+  });
 }
 
-module.exports = history
-
+module.exports = history;
