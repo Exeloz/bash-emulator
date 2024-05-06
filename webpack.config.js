726a11adfb050634a93dd46729d5112622733929
exeloz <christophercoulombe@gmail.com>
2024-05-06 17:16:50 -0400

    Changing browserify for webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/browser-version.js', // Your main entry point
+    output: {
+      filename: 'bundle.js', // Output filename
+      path: path.resolve(__dirname, 'dist'), // Output directory (optional)
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/, // Apply this rule to all JavaScript files
+          exclude: /node_modules/, // Exclude node_modules folder
+          use: {
+            loader: 'babel-loader', // Use Babel loader for transpilation
+            options: {
+              presets: ['@babel/preset-env'], // Preset for modern JS features
+            },
+          },
+        },
+      ],
+    },
+  };
+  
